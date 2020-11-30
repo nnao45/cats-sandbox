8ced57ee18dc5d6caf0f3d2238001c1e693e749f
nnao45 <n4sekai5y@gmail.com>
2020-11-30 21:30:44 +0900

    ok

--- src/main/scala/io/if_else/Main.scala
+++ src/main/scala/io/if_else/Main.scala
@@ -3,6 +3,9 @@ package io.if_else
 import cats.effect.IO
 import cats.syntax.all._
 
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
 object Main extends App {
 
   val func1 = IO(println("hey!!")) *> IO {
@@ -15,8 +18,28 @@ object Main extends App {
 
   val func3 = IO(func1.map(a => func2.map(_ + a)))
 
-  val trigger = true
-//  val trigger = false
+//  val trigger = true
+  val trigger = false
 
   println(if (trigger) 0 else func3.unsafeRunSync().unsafeRunSync().unsafeRunSync())
+
+  implicit val cx = ExecutionContext.global
+
+  val func4 = for {
+    _ <- Future(println("poi!!"))
+    ret <- Future {
+            1 + 2
+          }
+  } yield ret
+
+  val func5 = for {
+    _ <- Future(println("poipoi!!"))
+    ret <- Future {
+            2 + 3
+          }
+  } yield ret
+
+  val func6 = Future(func4.map(a => func5.map(_ + a)))
+
+  println(if (trigger) 0 else Await.result(func6, Duration.Inf))
 }
